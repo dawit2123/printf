413fc058f352cef05be6ae09e6e176b1ad127cb7
dawit2123 <dawitzewdu2123@gmail.com>
2023-01-24 15:21:20 +0300

    a funciton that gets the precision from the format string

--- string_fields.c
+++ string_fields.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_precision - gets the precision from the format string
+ * get_precision - a funciton that gets the precision from the format string
  * @p: the format string
  * @params: the parameters struct
  * @ap: the argument pointer
@@ -10,21 +10,21 @@
  */
 char *get_precision(char *p, params_t *params, va_list ap)
 {
-	int d = 0;
+	int num = 0;
 
 	if (*p != '.')
 		return (p);
 	p++;
 	if (*p == '*')
 	{
-		d = va_arg(ap, int);
+		num = va_arg(ap, int);
 		p++;
 	}
 	else
 	{
 		while (_isdigit(*p))
-			d = d * 10 + (*p++ - '0');
+			num = num * 10 + (*p++ - '0');
 	}
-	params->precision = d;
+	params->precision = num;
 	return (p);
 }
