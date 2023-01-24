8b3c693b2a10f996586c4d3e5324cc9ada50bb19
dawit2123 <dawitzewdu2123@gmail.com>
2023-01-24 14:46:57 +0300

    a function that prints everything

--- _printf.c
+++ _printf.c
@@ -9,11 +9,11 @@
 int _printf(const char *format, ...)
 {
 	int result = 0;
-	va_list ap;
+	va_list var;
 	char *p, *begin;
 	params_t params = PARAMS_INIT;
 
-	va_start(ap, format);
+	va_start(var, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
-		init_params(&params, ap);
+		init_params(&params, var);
 		if (*p != '%')
 		{
 			result += _putchar(*p);
@@ -33,17 +33,17 @@ int _printf(const char *format, ...)
 		{
 			p++; /* next char */
 		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
+		p = get_width(p, &params, var);
+		p = get_precision(p, &params, var);
 		if (get_modifier(p, &params))
 			p++;
 		if (!get_specifier(p))
 			result += print_from_to(begin, p,
 				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			result += get_print_func(p, ap, &params);
+			result += get_print_func(p, var, &params);
 	}
 	_putchar(BUF_FLUSH);
-	va_end(ap);
+	va_end(var);
 	return (result);
 }
